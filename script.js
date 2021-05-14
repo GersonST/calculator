90f3daf03d0d4eb2ebfeae81527488f55d4fde19
GersonST <gersonsevero90@gmail.com>
2021-05-14 19:59:55 -0300

    início do processo lógico

---
+++ script.js
@@ -0,0 +1,21 @@
+const num = [0,1,2,3,4,5,6,7,8,9] //variáveis dos números em Array
+
+
+
+//As operações básicas
+
+function soma() {
+    
+}
+function subtracao() {
+
+}
+function multiplicacao() {
+
+}
+function divisao() {
+
+}
+function igual() {
+
+}
